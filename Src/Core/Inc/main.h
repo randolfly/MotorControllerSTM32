39236b174ed9bc33a25dee2f0a0e4896e42e232e
Randolf <1665718426@qq.com>
2024-06-24 09:04:42 +0800

    init test stm32h743 hardware and software configuration

--- Src/Core/Inc/main.h
+++ Src/Core/Inc/main.h
@@ -57,6 +57,8 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define LED_1_Pin GPIO_PIN_15
+#define LED_1_GPIO_Port GPIOA
 
 /* USER CODE BEGIN Private defines */
 
